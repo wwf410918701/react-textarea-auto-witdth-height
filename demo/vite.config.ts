921892de089150a946b2ce7f59d8fd7d689d987c
Jimmy Wu <jimmy@cadmus.io>
2024-08-04 12:19:47 +1000

    docs(demo): update and keep auto-text-area package the latest version in demo
    Use auto-test-area in demo through resolve, instead of a external package, making sure is the latest
    version

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,4 +5,7 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: { "react-textarea-auto-witdth-height": "../../src" },
+  },
 });
